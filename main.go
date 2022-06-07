58ad964a466e2557153338068d3c511bf0f59083
John Hashem III <johnhashemiii@Johns-MacBook-Pro.local>
2022-06-07 16:52:50 -0400

    started struct

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
+//Create hardcoded vars for NP
+const apiVersion string = "networking.k8s.io/v1"
+
+type NetworkPolicy struct {
+}
+
 func main() {
-  fmt.Println("Hello World")
+	fmt.Println("YAML Here")
 }
