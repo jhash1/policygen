31c81942e85294c95b691c2c605a5f0f1354534f
John Hashem III <johnhashemiii@Johns-MacBook-Pro.local>
2022-06-07 17:06:49 -0400

    added main.go

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 
 //Create hardcoded vars for NP
 const apiVersion string = "networking.k8s.io/v1"
+const kind string = "NetworkPolicy"
 
 type NetworkPolicy struct {
+	name       string
+	namespace  string
+	labelName  string
+	labelValue string
+	policyType string
 }
 
 func main() {
