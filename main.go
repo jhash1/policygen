5d0b00d7bf7f8dc421f3c656b34d9a22acbb3e7b
John Hashem III <johnhashemiii@Johns-MacBook-Pro.local>
2022-06-08 17:58:35 -0400

    added in metadata fields

--- main.go
+++ main.go
@@ -7,34 +7,17 @@ import (
 )
 
 type NetworkPolicy struct {
-	APIVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	// Metadata   struct {
-	// 	Name      string `yaml:"name"`
-	// 	Namespace string `yaml:"namespace"`
-	// } `yaml:"metadata"`
-	// Spec struct {
-	// 	PodSelector struct {
-	// 		MatchLabels struct {
-	// 			App string `yaml:"app"`
-	// 		} `yaml:"matchLabels"`
-	// 	} `yaml:"podSelector"`
-	// 	PolicyTypes []string `yaml:"policyTypes"`
-	// 	Ingress     []struct {
-	// 		From []struct {
-	// 			PodSelector struct {
-	// 				MatchLabels struct {
-	// 					App string `yaml:"app"`
-	// 				} `yaml:"matchLabels"`
-	// 			} `yaml:"podSelector"`
-	// 		} `yaml:"from"`
-	// 	} `yaml:"ingress"`
-	// } `yaml:"spec"`
+	ApiVersion string            `yaml:"apiVersion"`
+	Kind       string            `yaml:"kind"`
+	Metadata   map[string]string `yaml:"metadata"`
 }
 
 func main() {
 	// Marshal a Person struct to YAML.
-	np := &NetworkPolicy{"networking.k8s.io/v1", "NetworkPolicy"}
+	np := &NetworkPolicy{"networking.k8s.io/v1",
+		"NetworkPolicy",
+		map[string]string{"name": "allowingresspolicy", "namespace": "insights-agent"},
+	}
 	y, err := yaml.Marshal(np)
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
