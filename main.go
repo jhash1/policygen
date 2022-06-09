5bb018886bc5ffaa3c1e338a2c607320db6e54d9
John Hashem III <johnhashemiii@Johns-MacBook-Pro.local>
2022-06-09 19:28:33 -0400

    added spec section

--- main.go
+++ main.go
@@ -10,16 +10,19 @@ type NetworkPolicy struct {
 	ApiVersion string            `yaml:"apiVersion"`
 	Kind       string            `yaml:"kind"`
 	Metadata   map[string]string `yaml:"metadata"`
-	//next part is coming up with the whole YAML such as the spec file,
-	//creating the framework for Cobra CLI and avail options. Ill add to spreadsheet
-	//add user input
+	Spec       map[string]string `yaml:"spec"`
 }
+
+//next part is coming up with the whole YAML such as the spec file,
+//creating the framework for Cobra CLI and avail options. Ill add to spreadsheet
+//add user input
 
 func main() {
 	// Marshal a NP struct to YAML.
 	np := &NetworkPolicy{"networking.k8s.io/v1",
 		"NetworkPolicy",
 		map[string]string{"name": "allowingresspolicy", "namespace": "insights-agent"},
+		map[string]string{"podSelector": "", "matchLabels": "{}"},
 	}
 	y, err := yaml.Marshal(np)
 	if err != nil {
